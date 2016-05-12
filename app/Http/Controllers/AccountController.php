9226a2271c8713a50ac023518d70935817520ea9
mariacrangel <maria.rangel@gmail.com>
2016-05-12 10:48:02 -0400

    more changes

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -138,46 +138,6 @@ class AccountController extends Controller
             /*Populate users extensions*/
             $this->generaExtension($ext_num, $secret);
             /*Until here*/
-            /*Populate Findmefollow*/
-            $findmefollow = new lb_findmefollow_asterisk;
-            $findmefollow->grpnum = $ext_num;
-            $findmefollow->strategy = 'ringallv2-prim';
-            $findmefollow->grptime = 20;
-            $findmefollow->grppre = '';
-            $findmefollow->grplist = $ext_num;
-            $findmefollow->annmsg_id = 0;
-            $findmefollow->postdest = 'ext-local,'.$ext_num.',dest';
-            $findmefollow->dring ='';
-            $findmefollow->remotealert_id =0;
-            $findmefollow->needsconf ='';
-            $findmefollow->toolate_id = 0;
-            $findmefollow->pre_ring = 7;
-            $findmefollow->ringing = 'Ring';
-            $findmefollow->save();
-            /*Until here*/
-             /*Populate incoming*/
-            $incoming = new lb_incoming_asterisk;
-            $incoming->cidnum = '';
-            $incoming->extension = $route_did['did'];
-            $incoming->destination =  'from-did-direct,'.$ext_num.',1';
-            $incoming->faxexten = NULL;
-            $incoming->faxemail = NULL;
-            $incoming->answer =NULL;
-            $incoming->wait =NULL;
-            $incoming->privacyman =0;
-            $incoming->alertinfo ='';
-            $incoming->ringing = '';
-            $incoming->mohclass = 'default';
-            $incoming->description = $account;
-            $incoming->grppre = '';
-            $incoming->delay_answer=0;
-            $incoming->pricid = '';
-            $incoming->pmmaxretries =3;
-            $incoming->pmminlength =10;
-            $incoming->reversal ='';
-            $incoming->save();
-            /*Until here*/
-            
             $this->scpConnect();
             //$this->sshConnect();
             
@@ -297,123 +257,12 @@ class AccountController extends Controller
         
     }
     
-    public function generateExtLocal($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $filename =  storage_path($file);
-        $search ='exten => vmret,1,GotoIf($["${IVR_RETVM}" = "RETURN" & "${IVR_CONTEXT}" != ""]?playret)';
-        $insert = PHP_EOL.'exten => *84'.$extension.',1,Goto(app-campon-toggle,*84,1)'.PHP_EOL.'exten => *84'.$extension.',hint,ccss:SIP/'.$extension.PHP_EOL.PHP_EOL.'exten => *'.$extension.',1,Macro(vm,'.$extension.',DIRECTDIAL,${IVR_RETVM})'.PHP_EOL.'exten => *'.$extension.',n,Goto(vmret,1)'.PHP_EOL.PHP_EOL.'exten => '.$extension.',1,Set(__RINGTIMER=${IF($["${DB(AMPUSER/'.$extension.'/ringtimer)}" > "0"]?${DB(AMPUSER/'.$extension.'/ringtimer)}:${RINGTIMER_DEFAULT})})'.PHP_EOL.PHP_EOL.'exten => '.$extension.',n,Macro(exten-vm,'.$extension.','.$extension.',0,0,0)'.PHP_EOL.'exten => '.$extension.',n(dest),Set(__PICKUPMARK=)'.PHP_EOL.'exten => '.$extension.',n,Macro(vm,'.$extension.',${DIALSTATUS},${IVR_RETVM})'.PHP_EOL.'exten => '.$extension.',n,Goto(vmret,1)'.PHP_EOL.'exten => '.$extension.',hint,SIP/'.$extension.'&Custom:DND'.$extension.',CustomPresence:'.$extension.PHP_EOL.'exten => vmb'.$extension.',1,Macro(vm,'.$extension.',BUSY,${IVR_RETVM})'.PHP_EOL.'exten => vmb'.$extension.',n,Goto(vmret,1)'.PHP_EOL.PHP_EOL.'exten => vmu'.$extension.',1,Macro(vm,'.$extension.',NOANSWER,${IVR_RETVM})'.PHP_EOL.'exten => vmu'.$extension.',n,Goto(vmret,1)'.PHP_EOL.'exten => vms'.$extension.',1,Macro(vm,'.$extension.',NOMESSAGE,${IVR_RETVM})'.PHP_EOL.'exten => vms'.$extension.',n,Goto(vmret,1)'.PHP_EOL.PHP_EOL.'exten => vmi'.$extension.',1,Macro(vm,'.$extension.',INSTRUCT,${IVR_RETVM})'.PHP_EOL.'exten => vmi'.$extension.',n,Goto(vmret,1)'.PHP_EOL.$search.PHP_EOL;
-       file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-    }
-    
-    public function generateFindMeFollow($extension)
-    {   
-        ini_set('memory_limit', '7000M');
-        $file = 'app/users/extensions_additional.conf';
-        $search = ';--== end of [ext-findmefollow] ==--;';
-        $filename =  storage_path($file);
-        
-        $insert = 'exten => *21'.$extension.',1,Goto(app-fmf-toggle,*21,1)'.PHP_EOL.'exten => *21'.$extension.',hint,Custom:FOLLOWME'.$extension.PHP_EOL.PHP_EOL.'exten => FM'.$extension.',1,Goto('.$extension.',FM'.$extension.')'.PHP_EOL.'exten => '.$extension.',1,GotoIf($[ "${DB(AMPUSER/'.$extension.'/followme/ddial)}" = "EXTENSION" ]?ext-local,'.$extension.',1)'.PHP_EOL.'exten => '.$extension.',n(FM'.$extension.'),Macro(user-callerid,)'.PHP_EOL.'exten => '.$extension.',n,Set(DIAL_OPTIONS=${DIAL_OPTIONS}I)'.PHP_EOL.'exten => '.$extension.',n,Set(CONNECTEDLINE(num,i)='.$extension.')'.PHP_EOL.'exten => '.$extension.',n,Gosub(sub-presencestate-display,s,1('.$extension.'))'.PHP_EOL.'exten => '.$extension.',n,Set(CONNECTEDLINE(name)=${DB(AMPUSER/'.$extension.'/cidname)}${PRESENCESTATE_DISPLAY})'.PHP_EOL.'exten => '.$extension.',n,Set(FM_DIALSTATUS=${EXTENSION_STATE('.$extension.'@ext-local)})'.PHP_EOL.'exten => '.$extension.',n,Set(__EXTTOCALL=${EXTEN})'.PHP_EOL.'exten => '.$extension.',n,Set(__PICKUPMARK=${EXTEN})'.PHP_EOL.'exten => '.$extension.',n,Macro(blkvm-setifempty,)'.PHP_EOL.'exten => '.$extension.',n,GotoIf($["${GOSUB_RETVAL}" = "TRUE"]?skipov)'.PHP_EOL.'exten => '.$extension.',n,Macro(blkvm-set,reset)'.PHP_EOL.'exten => '.$extension.',n,Set(__NODEST=)'.PHP_EOL.'exten => '.$extension.',n(skipov),Set(RRNODEST=${NODEST})'.PHP_EOL.'exten => '.$extension.',n(skipvmblk),Set(__NODEST=${EXTEN})'.PHP_EOL.'exten => '.$extension.',n,GosubIf($[${DB_EXISTS(AMPUSER/'.$extension.'/followme/changecid)} = 1 & "${DB(AMPUSER/'.$extension.'/followme/changecid)}" != "default" & "${DB(AMPUSER/'.$extension.'/followme/changecid)}" != ""]?sub-fmsetcid,s,1())'.PHP_EOL.'exten => '.$extension.',n,Set(RecordMethod=Group)'.PHP_EOL.'exten => '.$extension.',n(checkrecord),Gosub(sub-record-check,s,1(exten,'.$extension.',))'.PHP_EOL.'exten => '.$extension.',n(skipsimple),Set(RingGroupMethod=ringallv2-prim)'.PHP_EOL.'exten => '.$extension.',n,Set(_FMGRP='.$extension.')'.PHP_EOL.'exten => '.$extension.',n(DIALGRP),GotoIf($[("${DB(AMPUSER/'.$extension.'/followme/grpconf)}"="ENABLED") | ("${FORCE_CONFIRM}"!="") ]?doconfirm)'.PHP_EOL.'exten => '.$extension.',n,Macro(dial,$[ ${DB(AMPUSER/'.$extension.'/followme/grptime)} + ${DB(AMPUSER/'.$extension.'/followme/prering)} ],${DIAL_OPTIONS},${DB(AMPUSER/'.$extension.'/followme/grplist)})'.PHP_EOL.'exten => '.$extension.',n,Goto(nextstep)'.PHP_EOL.'exten => '.$extension.',n(doconfirm),Macro(dial-confirm,$[ ${DB(AMPUSER/'.$extension.'/followme/grptime)} + ${DB(AMPUSER/'.$extension.'/followme/prering)} ],${DIAL_OPTIONS},${DB(AMPUSER/'.$extension.'/followme/grplist)},'.$extension.')'.PHP_EOL.'exten => '.$extension.',n(nextstep),Set(RingGroupMethod=)'.PHP_EOL.'exten => '.$extension.',n,GotoIf($["foo${RRNODEST}" != "foo"]?nodest)'.PHP_EOL.'exten => '.$extension.',n,Set(__NODEST=)'.PHP_EOL.'exten => '.$extension.',n,Set(__PICKUPMARK=)'.PHP_EOL.'exten => '.$extension.',n,Macro(blkvm-clr,)'.PHP_EOL.'exten => '.$extension.',n,Set(DIALSTATUS=${IF($["${FM_DIALSTATUS}"="NOT_INUSE"&"${DIALSTATUS}"!="CHANUNAVAIL"]?NOANSWER:${IF($["${DIALSTATUS}"="CHANUNAVAIL"|"${FM_DIALSTATUS}"="UNAVAILABLE"|"${FM_DIALSTATUS}"$'.PHP_EOL.'exten => '.$extension.',n,Goto(ext-local,'.$extension.',dest)'.PHP_EOL.'exten => '.$extension.',n(nodest),Noop(SKIPPING DEST, CALL CAME FROM Q/RG: ${RRNODEST})'.PHP_EOL.$search.PHP_EOL;
-
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-
-    }
-    
-    public function generateFmgrps($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $filename =  storage_path($file);
-        
-        $search = ';--== end of [fmgrps] ==--;';
-        
-        $insert = PHP_EOL.'exten => _RG-'.$extension.'.,1,NoCDR()'.PHP_EOL.'exten => _RG-'.$extension.'.,n,Macro(dial,${DB(AMPUSER/'.$extension.'/followme/grptime)},${DIAL_OPTIONS}M(confirm^^^'.$extension.'),${EXTEN:7})'.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-    }
-    public function generateHints($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $search =';--== end of [ext-cf-hints] ==--;';
-        $filename =  storage_path($file);
-        $insert = PHP_EOL.'exten => *96'.$extension.',1,Goto(app-cf-toggle,*96,1)'.PHP_EOL.'exten => *96'.$extension.',hint,Custom:DEVCF'.$extension.PHP_EOL.'exten => _*96'.$extension.'.,1,Set(toext=${EXTEN:6})'.PHP_EOL.'exten => _*96'.$extension.'.,n,Goto(app-cf-toggle,*96,setdirect)'.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-        
-    }
-    public function generateDndHints($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $search =';--== end of [ext-dnd-hints] ==--;';
-        $filename =  storage_path($file);
-        $insert = PHP_EOL.'exten => *76'.$extension.',1,Goto(app-dnd-toggle,*76,1)'.$extension.PHP_EOL.'exten => *76'.$extension.',hint,Custom:DEVDND'.$extension.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-        
-    }
-    
-    public function generateIntercom($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $search = ';--== end of [ext-intercom-users] ==--;';
-        $filename =  storage_path($file);
-        $insert = 'exten => *80'.$extension.',1,Goto(ext-intercom,${EXTEN},1)'.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-   
-    }
-    
-    public function generateIvr($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $search = ';--== end of [from-did-direct-ivr] ==--;';
-        $filename =  storage_path($file);
-        $insert = PHP_EOL.'exten => *'.$extension.',1,Macro(blkvm-clr,)'.PHP_EOL.'exten => *'.$extension.',n,Set(__NODEST=)'.PHP_EOL.'exten => *'.$extension.',n,Macro(vm,'.$extension.',DIRECTDIAL,${IVR_RETVM})'.PHP_EOL.'exten => *'.$extension.',n,GotoIf($["${IVR_RETVM}" = "RETURN" & "${IVR_CONTEXT}" != ""]?ext-local,vmret,playret)'.PHP_EOL.'exten => '.$extension.',1,Macro(blkvm-clr,)'.PHP_EOL.'exten => '.$extension.',n,Set(__NODEST=)'.PHP_EOL.'exten => '.$extension.',n,Goto(from-did-direct,'.$extension.',1)'.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));
-    }
-    
-    public function generateDid($did, $extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $filename =  storage_path($file);
-        $search =';--== end of [ext-did-0002] ==--;';
-        $insert = PHP_EOL.'exten => '.$did.',1,Set(__FROM_DID=${EXTEN})'.PHP_EOL.'exten => '.$did.',n,Gosub(app-blacklist-check,s,1())'.PHP_EOL.'exten => '.$did.',n,Set(CDR(did)=${FROM_DID})'.PHP_EOL.'exten => '.$did.',n,ExecIf($[ "${CALLERID(name)}" = "" ] ?Set(CALLERID(name)=${CALLERID(num)}))'.PHP_EOL.'exten => '.$did.',n,Set(CHANNEL(musicclass)=default)'.PHP_EOL.'exten => '.$did.',n,Set(__MOHCLASS=default)'.PHP_EOL.'exten => '.$did.',n,GotoIf($["${__REVERSAL_REJECT}"="TRUE" & "${CHANNEL(reversecharge)}"="1" ]?macro-hangupcall)'.PHP_EOL.'exten => '.$did.',n,Set(__CALLINGPRES_SV=${CALLERPRES()})'.PHP_EOL.'exten => '.$did.',n,Set(CALLERPRES()=allowed_not_screened)'.PHP_EOL.'exten => '.$did.',n(dest-ext),Goto(from-did-direct,'.$extension.',1)'.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename)));    
-    }
-    public function generateVoicemail($extension)
-    {
-        $file = 'app/users/voicemail.conf';
-        $filename =  storage_path($file);
-        $insert = PHP_EOL.$extension.' => 123456,'.$extension.',,,attach=no|saycid=no|envelope=no|delete=no'.PHP_EOL;
-        file_put_contents($filename, $insert, FILE_APPEND);
-    }
-    
-     public function generateParked($extension)
-    {
-        $file = 'app/users/extensions_additional.conf';
-        $filename =  storage_path($file);
-        $search =';--== end of [park-hints] ==--;';
-        $insert = PHP_EOL.'exten => *85'.$extension.',1,Macro(parked-call,,default)'.PHP_EOL.'exten => *85'.$extension.',hint,Custom:PARK'.$extension.PHP_EOL.$search.PHP_EOL;
-        
-        file_put_contents($filename, str_replace($search, $insert, file_get_contents($filename))); 
-    }
-    
-     public function generateSip($extension, $account, $secret)
-    {
-        $file = 'app/users/sip_additional.conf';
-        $filename =  storage_path($file);
-        $insert = '['.$extension.']'.PHP_EOL.'deny=0.0.0.0/0.0.0.0'.PHP_EOL.'secret='.$secret.PHP_EOL.'dtmfmode=rfc2833'.PHP_EOL.'canreinvite=no'.PHP_EOL.'context=from-internal'.PHP_EOL.'host=dynamic'.PHP_EOL.'trustrpid=yes'.PHP_EOL.'mediaencryption=no'.PHP_EOL.'sendrpid=pai'.PHP_EOL.'type=friend'.PHP_EOL.'nat=force_rport,comedia'.PHP_EOL.'port=5060'.PHP_EOL.'qualify=yes'.PHP_EOL.'qualifyfreq=60'.PHP_EOL.'transport=udp,tcp,tls'.PHP_EOL.'avpf=no'.PHP_EOL.'force_avp=no'.PHP_EOL.'icesupport=no'.PHP_EOL.'encryption=no'.PHP_EOL.'callgroup='.PHP_EOL.'pickupgroup='.PHP_EOL.'dial=SIP/'.$extension.PHP_EOL.'mailbox='.$extension.'@default'.PHP_EOL.'permit=0.0.0.0/0.0.0.0'.PHP_EOL.'callerid='.$account.' <'.$extension.'>'.PHP_EOL.'callcounter=yes'.PHP_EOL.'faxdetect=no'.PHP_EOL;
-
-                file_put_contents($filename, $insert, FILE_APPEND);
-
- }
-    
+     
     public function scpConnect()
     {
         return SSH::into('asterisk')->run(array(
-               'scp root@linebacker.privacyprotector.org:/var/www/backend/storage/app/users/*  /etc/asterisk/',
-               'chown -R asterisk.asterisk /etc/asterisk/',
+            //   'scp root@linebacker.privacyprotector.org:/var/www/backend/storage/app/users/*  /etc/asterisk/',
+            //   'chown -R asterisk.asterisk /etc/asterisk/',
                '/etc/init.d/asterisk reload',
                'exit'
               ));
