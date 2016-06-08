e66a6bd1144b9d40123192213fcea167c82a651f
mariacrangel <maria.rangel@gmail.com>
2016-06-08 19:22:39 -0400

    fix 3

--- resources/views/users/account/index.blade.php
+++ resources/views/users/account/index.blade.php
@@ -16,26 +16,5 @@
             </thead>
             <tbody>
                 {{ count($account) }}
-            <!--@if(count($account)>0)
-            {{-- */$x=0;/* --}}
-            @foreach($account as $item)
-                {{-- */$x++;/* --}}
-                <tr>
-                    <td>{{ $x }}</td>
-                    <td><a href="{{ url('users/account/show/'. $item->userAcc) }}">{{ $item->id_membership }}</a></td><td>{{ $item->userAcc }}</td><td>{{ $item->id_city }}</td><td>{{ $item->first_name }}</td>
-                    <td>
-                        <a href="{{ url('users/account/edit/' . $item->userAcc) }}"><span class="fa fa-pencil-square-o" aria-hidden="true"></span></a> /
-                        
-                        <a title="Destroy" href="{{ URL::to('users/account/destroy/' . $item->userAcc ) }}"><span class="fa fa-trash" aria-hidden="true"></span></a>
-                        
-                        
-                    </td>
-                </tr>
-            @endforeach
-            @endif
-            </tbody>
-        </table>
-        <div class="pagination"> @if(count($account)>0) {!! $account->render() !!} @endif </div>
-    </div>
--->
+
 @endsection
