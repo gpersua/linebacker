91fd8f7ecbab8e0cd366275820c610905bd15724
mariacrangel <maria.rangel@gmail.com>
2016-06-14 16:33:57 -0400

    leyenda 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,11 +12,11 @@
 			<div class="panel panel-default">
 				<div class="panel-heading"></div>
 
-				<div class="panel-body">
+				<!--<div class="panel-body">-->
                                     <div class="col-md-3">
                                         <a href="{{ asset('/users/account/create') }}"><img src="{{ asset('/assets/img/telefono-icon-grande.png') }}"><br /> Create an account extension</a>	
                                     </div>
-				</div>
+				<!--</div>-->
 			</div>
 		</div>
 	</div>
