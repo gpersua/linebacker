f7baf3d7b8b8826dc6cc24687e4af89c4233c8e8
mariacrangel <maria.rangel@gmail.com>
2016-06-14 16:25:25 -0400

    otros Cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,12 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading"></div>
 
 				<div class="panel-body">
+                                    <div class="col-md-3">
                                     <a href="{{ asset('/users/account/create') }}"><img src="{{ asset('/assets/img/telefono-icon-grande.png') }}"></a>	
+                                    </div>
 				</div>
 			</div>
 		</div>
