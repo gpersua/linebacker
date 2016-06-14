8ac16b42ff52e62e0995c2a05e74762f4ef66be7
mariacrangel <maria.rangel@gmail.com>
2016-06-14 16:35:07 -0400

    leyenda 3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,9 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading"></div>
+				<!--<div class="panel-heading"></div>
 
-				<!--<div class="panel-body">-->
+				<div class="panel-body">-->
                                     <div class="col-md-3">
                                         <a href="{{ asset('/users/account/create') }}"><img src="{{ asset('/assets/img/telefono-icon-grande.png') }}"><br /> Create an account extension</a>	
                                     </div>
