7396987e595eb83549c0584c1b23392c67166b97
mariacrangel <maria.rangel@gmail.com>
2016-06-13 22:40:04 -0400

    cabios quitar notificaciones

--- resources/views/partials/mainheader.blade.php
+++ resources/views/partials/mainheader.blade.php
@@ -18,7 +18,7 @@
                 <!-- Messages: style can be found in dropdown.less-->
                 <li class="dropdown messages-menu">
                     <!-- Menu toggle button -->
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  <!--  <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <i class="fa fa-envelope-o"></i>
                         <span class="label label-success">4</span>
                     </a>
