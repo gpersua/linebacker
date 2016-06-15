8d15166e1077404dbe2367c9870b0f1d1ae49a3b
mariacrangel <maria.rangel@gmail.com>
2016-06-15 12:13:05 -0400

    video codecs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -126,7 +126,7 @@
                     <ul class="slides">
                         <li>
                             <video id="video" loop="" width="100%" height="auto" autoplay="true">
-                            <source src="http://linebacker.privacyprotector.org/assets/img/bannerproject720p.flv.mp4" type="video/mp4">
+                            <source src="http://linebacker.privacyprotector.org/assets/img/bannerproject720p.flv.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2">
                                             </video>
             </li>
                     </ul>
