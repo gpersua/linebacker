74c83b0aca1c1a3dde0d09dbaef531b134573201
mariacrangel <maria.rangel@gmail.com>
2016-06-22 17:40:41 -0400

    video4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,7 +132,7 @@ video {
                 <div class="flexslider">
                     <ul class="slides">
                         <li>
-                            <video controls height="auto" poster="/tests/images/BBB_480_Poster.jpg" src="http://linebacker.privacyprotector.org/assets/img/bannerproject720p.flv.mp4" width="100%"></video>
+                            <video controls height="auto" poster="/tests/images/BBB_480_Poster.jpg" src="http://linebacker.privacyprotector.org/assets/img/bannerproject720p.flv.mp4" width="auto"></video>
                             <!--<video id="video" loop="" width="100%" height="auto" autoplay="true">
                             <source src="http://linebacker.privacyprotector.org/assets/img/bannerproject720p.flv.mp4" type="video/mp4" codecs="avc1.42E01E, mp4a.40.2">
                                             </video>-->
