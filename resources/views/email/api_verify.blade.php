e27a2b906adb4390b8bbd85e7d416729f6a77b07
mariacrangel <maria.rangel@gmail.com>
2016-06-24 02:59:51 -0400

    Correccion en la ruta de confirmación de email

--- resources/views/email/api_verify.blade.php
+++ resources/views/email/api_verify.blade.php
@@ -9,7 +9,7 @@
         <div>
             Thanks for creating an account with the verification demo app.
             Please follow the link below to verify your email address
-            {{ URL::to('api/register/' . $confirmation_code['confirmation_code']) }}.<br/>
+            {{ URL::to('api/confirmation/' . $confirmation_code['confirmation_code']) }}.<br/>
 
         </div>
 
