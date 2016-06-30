e45d68a534fc904c83a099d4dad75f6c982ee855
mariacrangel <maria.rangel@gmail.com>
2016-06-30 06:45:27 -0400

    in_active changes

--- app/Http/Controllers/Auth/PagesController.php
+++ app/Http/Controllers/Auth/PagesController.php
@@ -20,7 +20,7 @@ class PagesController extends Controller {
     {
         // $confirmed = DB::table('lb_users')->where('email', Input::get('email'))->value('confirmed');
          //var_dump($confirmed);
-	if (Auth::check() && Auth::User()->confirmed){
+	if (Auth::check() && Auth::User()->confirmed && Auth::User()->confirmed){
                /* if($account = DB::table('lb_account')->where('id', Auth::User()->id)->value('userAcc'))
                 {
                     Session::put('userAcc', $account);
@@ -30,7 +30,7 @@ class PagesController extends Controller {
 	}
         Auth::logout();
         Session::flush();
-        return View::make('welcome');
+        return View::make('auth/login');
     }
     
     public function welcome()
