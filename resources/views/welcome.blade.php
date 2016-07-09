873ea62150118534e6dd7e35409000376fb62bad
mariacrangel <maria.rangel@gmail.com>
2016-07-09 15:03:04 -0400

    front fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -414,7 +414,7 @@
 						<li class="wow fadeIn"><a href="#">Track my case</a></li>
 					</ol>
                     <div class="footer-copyright wow fadeIn">
-                        <p>&copy; 2015 LineBacker. All rights reserved. Designed and Developed by <a href="https://www.clouditapp.com"><img src="{{ asset('/assets/img/cloudlogo.png') }}"/></a></p>
+                        <p>&copy; 2015 LineBacker. All rights reserved. Designed and Developed by <a href="https://www.cmsysconsulting.com"><img src="{{ asset('/assets/img/logo_cmsys.png') }}"/></a></p>
                     </div>                   
                 </div>
             </div>
