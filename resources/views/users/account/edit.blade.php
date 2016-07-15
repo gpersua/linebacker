62c50a9af596d6600d7e711ee67b9238399a7f4f
mariacrangel <maria.rangel@gmail.com>
2016-07-14 23:17:57 -0400

    modified:   resources/views/users/account/edit.blade.php

--- resources/views/users/account/edit.blade.php
+++ resources/views/users/account/edit.blade.php
@@ -105,7 +105,7 @@
             <div class="form-group {{ $errors->has('birthday') ? 'has-error' : ''}}">
                 {!! Form::label('birthday', 'Birthday: ', ['class' => 'col-sm-3 control-label']) !!}
                 <div class="col-sm-6">
-                    {!! Form::text('birthday', null, ['id' => 'birthday','class' => 'form-control datepicker', 'required' => 'required']) !!}
+                    {!! Form::text('birthday', null, ['id' => 'birthday','class' => 'form-control datepicker', 'required' => 'required','data-provide' => 'datepicker']) !!}
                     {!! $errors->first('birthday', '<p class="help-block">:message</p>') !!}
                 </div>
             </div>
