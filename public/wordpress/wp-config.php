c139475373a0b1016ca73170bd52a1343b9a15bd
Luis Ugueto <blink242@outlook.com>
2016-07-19 17:37:07 -0430

    Conexion Wordpress

--- public/wordpress/wp-config.php
+++ public/wordpress/wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'http://localhost');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
